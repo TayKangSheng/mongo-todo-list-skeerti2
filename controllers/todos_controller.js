787ce13e42707773fe808224c0e19e1fef63bff2
Sruti Keerti <skeerti2@gmail.com>
2017-02-09 18:32:25 +0800

    assignment completed

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,20 +1,69 @@
+
 // TODO. import TODO Model ;-)
-// const Todo = require('../models/todo')
+const Todo = require('../models/todo')
 
 function create (params) {
   // create a new TODO and console log the response
+  Todo.create(params, function(err, createdobj){
+    if(err){
+      console.error(err)
+      return
+    }
+    console.log(createdobj)
+  })
 }
 function list () {
   // console log the list of all TODOs
+Todo.find({}, function(err, todos){
+  if(err){
+    console.error(err)
+    return
+  }
+  console.log(todos)
+})
 }
 function show (id) {
   // find the TODO with this id and console log it
+  Todo.findById((id), function(err, object){
+    if(err){
+      console.error(err)
+      return
+    }
+    console.log(object)
+  })
 }
 function update (id, params) {
   // find the TODO with this id and update it's params. console log the result.
+  Todo.findByIdAndUpdate((id), (params),{new:true}, function(err, updatedObj){
+    if(err){
+      console.error(err)
+      return
+    }
+    console.log(updatedObj)
+  })
 }
 function destroy (id) {
   // find the TODO with this id and destroy it. console log success/failure.
+  Todo.findByIdAndRemove((id), function(err, removedObj){
+    if(err)
+    {
+      console.error(err)
+      console.log('failure')
+      return
+    }
+    console.log(removedObj)
+    console.log('success')
+  })
+}
+
+function destroyAll () {
+  Todo.remove({},function(err,noObj){
+    if(err){
+      console.error(err)
+      return
+    }
+  })
+    console.log('All items have been deleted')
 }
 
 module.exports = {
@@ -22,5 +71,6 @@ module.exports = {
   list,
   show,
   update,
-  destroy
+  destroy,
+  destroyAll
 }
--- models/todo.js
+++ models/todo.js
@@ -1,1 +1,13 @@
 // Mongoose Schema and Models goes here
+const mongoose = require('mongoose')
+// mongoose.connect('mongodb://localhost/movies')
+// mongoose.Promise = global.promise
+
+const todoSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  completed: Boolean
+})
+
+const Todo = mongoose.model('Todo', todoSchema)
+module.exports = Todo
